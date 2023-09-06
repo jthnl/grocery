41366572ee51f1be312dc73cb9f9553f3854aa1b
Jath Ong <ong.jathniel@gmail.com>
2023-09-05 22:31:31 -0700

    config db

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -14,6 +14,7 @@ export const conn = knex({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
+    ssl: true,
   },
 });
 
